2003e281e5f5b9a40b1883af992bf645d4780876
Guilherme Ribeiro <augusto.gui.2208@gmail.com>
2021-08-02 17:12:50 -0300

    CardMovie :construction:

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import CardMovie from "./components/CardMovie/CardMovie";
 import Header from "./components/Header/Header";
 import Info from "./components/Info/Info";
 
 function App() {
   return (
     <>
-    <Header />
-    <Info />
+      <Header />
+      <Info />
+      <CardMovie />
     </>
   );
 }
---
+++ src/components/CardMovie/CardMovie.jsx
@@ -0,0 +1,11 @@
+import { Main } from "./Style";
+
+const CardMovie = () => {
+  return (
+    <Main>
+      <h1>Hello World</h1>
+    </Main>
+  );
+};
+
+export default CardMovie;
