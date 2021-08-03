c960c3602a6d2d21e5887a29fe2c5e84640e06f0
Guilherme Ribeiro <augusto.gui.2208@gmail.com>
2021-08-03 00:05:12 -0300

    Layout

--- src/components/CardMovie/CardMovie.jsx
+++ src/components/CardMovie/CardMovie.jsx
@@ -3,7 +3,74 @@ import { Main } from "./Style";
 const CardMovie = () => {
   return (
     <Main>
-      <h1>Hello World</h1>
+      <div className="card">
+        <div className="card__info">
+          <div className="card__img">
+            <img src="https://picsum.photos/200" alt="Imagem do filme" />
+            <div>Movie</div>
+          </div>
+          <div className="movie">
+            <div>
+              <span className="movie__countrie">Usa, </span>
+              <span className="movie__date">2016</span>
+            </div>
+            <span className="movie__name">Stranger Things</span>
+            <span className="movie__votes">8/10</span>
+            <span className="movie__type">Action/Adventure, Horror</span>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card__info">
+          <div className="card__img">
+            <img src="https://picsum.photos/200" alt="Imagem do filme" />
+            <div>Movie</div>
+          </div>
+          <div className="movie">
+            <div>
+              <span className="movie__countrie">Usa, </span>
+              <span className="movie__date">2016</span>
+            </div>
+            <span className="movie__name">Stranger Things</span>
+            <span className="movie__votes">8/10</span>
+            <span className="movie__type">Action/Adventure, Horror</span>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card__info">
+          <div className="card__img">
+            <img src="https://picsum.photos/200" alt="Imagem do filme" />
+            <div>Movie</div>
+          </div>
+          <div className="movie">
+            <div>
+              <span className="movie__countrie">Usa, </span>
+              <span className="movie__date">2016</span>
+            </div>
+            <span className="movie__name">Stranger Things</span>
+            <span className="movie__votes">8/10</span>
+            <span className="movie__type">Action/Adventure, Horror</span>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card__info">
+          <div className="card__img">
+            <img src="https://picsum.photos/200" alt="Imagem do filme" />
+            <div>Movie</div>
+          </div>
+          <div className="movie">
+            <div>
+              <span className="movie__countrie">Usa, </span>
+              <span className="movie__date">2016</span>
+            </div>
+            <span className="movie__name">Stranger Things</span>
+            <span className="movie__votes">8/10</span>
+            <span className="movie__type">Action/Adventure, Horror</span>
+          </div>
+        </div>
+      </div>
     </Main>
   );
 };
--- src/components/CardMovie/Style.js
+++ src/components/CardMovie/Style.js
@@ -1,3 +1,79 @@
 import styled from "styled-components";
 
-export const Main = styled.main``;
+export const Main = styled.main`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 25px;
+
+  max-width: 1358px;
+  margin: 30px auto;
+
+
+  .card__info {
+    display: flex;
+    flex-direction: column;
+    /* align-items: center; */
+    border-radius: 8px;
+
+    width: 319px;
+    height: 402px;
+    background-color: #334756;
+
+    .card__img {
+      max-width: 100%;
+      position: relative;
+      
+      img {
+        width: 100%;
+        max-height: 215px;
+        border-radius: 8px;
+      }
+
+      > div{
+        font-weight: bold;
+
+        width: 95px;
+        height: 42px;
+        background-color: #9BA4B4;
+        border-radius: 10px;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        position: absolute;
+        top: 1rem;
+        left: 1rem;
+      }
+    }
+    
+    .movie {
+      display: flex;
+      flex-direction: column;
+      margin-top: 28px;
+      padding: 0 15px;
+
+      font-size: 0.9rem;
+      font-weight: 500;
+
+      > div {
+        margin-bottom: 15px;
+      }
+
+      & span + span {
+        margin-top: 15px;
+      }
+
+      .movie__type {
+        font-size: 0.8rem;
+        color: #8D93AB;
+      }
+    }
+
+    @media(max-width: 350px) {
+      width: 300px;
+    }
+  }
+`;
