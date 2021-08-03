b8c1dce27deb2ce4825c47c84021146ba4904378
Guilherme Ribeiro <augusto.gui.2208@gmail.com>
2021-08-03 17:24:03 -0300

    ideia api

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
+function teste({ page }) {
+  axios.create({
+    baseURL: `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=40698a7bda352049c103b665527f1793&page=${page}`,
+  });
+}
+
 const api = axios.create({
-  baseURL: `https://api.themoviedb.org/3/discover/movie?api_key=40698a7bda352049c103b665527f1793&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false&with_watch_monetization_types=flatrate`,
+  baseURL: `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=40698a7bda352049c103b665527f1793&page=1`,
   // https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=40698a7bda352049c103b665527f1793&page=1
   // https://api.themoviedb.org/3/discover/movie?api_key=40698a7bda352049c103b665527f1793&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false&with_watch_monetization_types=flatrate
 });
